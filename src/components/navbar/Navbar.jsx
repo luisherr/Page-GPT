6740766031f41e8895abdebc17f19aa2197e2ea3
Luis Herrera <luis378962@gmail.com>
2023-03-08 16:08:33 -0600

    Header finalizado

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -23,7 +23,11 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <Menu />
+        <p><a href="#ahome">Home</a></p>
+        <p><a href="#wgpt3">What is GPT-3</a></p>
+        <p><a href="#possibility">Open AI</a></p>
+        <p><a href="#features">Case Studies</a></p>
+        <p><a href="#blog">Library</a></p>
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
